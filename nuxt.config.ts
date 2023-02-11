f43b50b9c6e51c6b4348320e2551b58a26fec612
ru44 <18311071+ru44@users.noreply.github.com>
2023-02-11 22:19:28 +0300

    del placeholder files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss'],
+  ssr: true,
+  experimental: {
+  reactivityTransform: false,
+},
+// auto import components
+components: true,
+  modules: ["@vueuse/nuxt", "@nuxtjs/tailwindcss"],
   vite: {
     define: {
       'process.env.DEBUG': false,
